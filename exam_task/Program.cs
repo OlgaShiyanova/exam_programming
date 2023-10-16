ab56cf7f93417e0bd1a114c3f1c20f4527949b63
ShiyanovaOlga <b-oa@yandex.ru>
2023-10-16 11:37:00 +0300

    контрольная задача работает

--- exam_task/Program.cs
+++ exam_task/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите размер массива: ");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+
+// Проверка длины массива
+if(sizeArray <= 0)
+{
+    Console.WriteLine("массив нулевой длины, попробуйте еще раз.");
+    return;
+}
+
+// Создаем массивы
+string[] array = new string[sizeArray];
+string[] newArray = new string[array.Length];
+
+// Собираем массив с клавиатуры
+void FillArray(string[] arr)
+{
+    Console.WriteLine("Введите элементы массива: ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"Элемент {i + 1} = ", i);
+        arr[i] = (Console.ReadLine()!);
+    }
+}
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}  ");
+    }
+}
+
+// Создание нового массива с выполнением условия
+void CreateNewArray(string[] arr1, string[] arr2)
+{
+    int index = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        if (arr1[i].Length <= 3)
+        {
+            arr2[index] = arr1[i];
+            index++;
+        }
+    }
+}
+
+FillArray(array);
+Console.Write("Первичный массив -> ");
+PrintArray(array);
+Console.WriteLine();
+CreateNewArray(array, newArray);
+Console.Write("Конечный массив -> ");
+PrintArray(newArray);
